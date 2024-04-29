0450a0d32810eda821d69f1a9d5f0ca939bccde9
rmucheri <vamucheri@gmail.com>
2024-04-29 13:42:16 -0600

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { rufus, rufustech1 } from '../assets'
 
+const currentYear = new Date().getFullYear();
+
 function Footer() {
   return (
     <div className='bg-[#0B123F]'>
@@ -66,16 +68,17 @@ function Footer() {
   </div>
   <div class="bg-gray-100">
     <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-      <p class="text-gray-500 text-sm text-center sm:text-left">© Rufaro Dev
+      <p class="text-gray-500 text-sm text-center sm:text-left"> © Copyright {currentYear} Rufaro Mucheri. All rights reserved. 
         <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank"></a>
       </p>
       <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-        <a class="text-gray-500">
+      <p className="text-xs">Powered by RufusTech</p>
+        {/* <a class="text-gray-500">
           <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
             <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
           </svg>
-        </a>
-        <a class="ml-3 text-gray-500">
+        </a> */}
+        {/* <a class="ml-3 text-gray-500">
           <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
             <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
           </svg>
@@ -91,10 +94,11 @@ function Footer() {
             <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
             <circle cx="4" cy="4" r="2" stroke="none"></circle>
           </svg>
-        </a>
+        </a> */}
       </span>
     </div>
   </div>
+ 
 </footer>
     </div>
   )
